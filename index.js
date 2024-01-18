0771188abfbc57e6ff11bdba43c5197b3db23848
DOUAR Sara <sarahdr912@gmail.com>
2024-01-18 15:18:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+let nameEl = document.getElementById("name")
+let userEl =document.getElementById("user")
+let locationEl = document.getElementById("location")
+let avatarEl = document.getElementById("avatar")
+let postEl = document.getElementById("post")
+let commentEl = document.getElementById("comment")
+let likesEl = document.getElementById("likes")
+
+// const heartEl = document.getElementById("heart-icon")
+
+//  let count = 0
+// heartEl.addEventListener("click", function (){
+   
+//     count +=1 
+//     likesEl.textContent = `${count} likes` 
+// })
+
+// let counts = 0
+// postEl.addEventListener("dblclick", function (){
+   
+//      counts +=1 
+//      likesEl.textContent = `${counts} likes` 
+     
+// })
+
+
+
+
+
+    
+
+      
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
